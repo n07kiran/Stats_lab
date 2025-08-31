1e90f0e868a91757b1b83d2c90590df46b6393cc
n07kiran <90110992+n07kiran@users.noreply.github.com>
2025-08-31 11:47:53 +0530

    Update lab1.R
    Added question

--- lab1.R
+++ lab1.R
@@ -1,3 +1,6 @@
+# Extract a dataset of your choice and compute the correlation between any two variables 
+# and visualize the relationship using scatter plot using JMP Pro tool. 
+
 data()
 mtcars
 
